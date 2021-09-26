9e34f31e08cb2e2218bc440361107999f31bba13
maihao <mohuaihaoxm@163.com>
2021-09-26 21:08:20 +0800

    增加了在详情界面直接切换菜谱功能

--- LittleRecipes/app/src/main/java/com/maihao/littlerecipes/activity/MainActivity.java
+++ LittleRecipes/app/src/main/java/com/maihao/littlerecipes/activity/MainActivity.java
@@ -9,7 +9,6 @@ import com.maihao.littlerecipes.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- LittleRecipes/app/src/main/java/com/maihao/littlerecipes/activity/RecipeActivity.java
+++ LittleRecipes/app/src/main/java/com/maihao/littlerecipes/activity/RecipeActivity.java
@@ -1,16 +1,37 @@
 package com.maihao.littlerecipes.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.maihao.littlerecipes.R;
+import com.maihao.littlerecipes.databinding.ActivityRecipeBinding;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton button;
+
+    ActivityRecipeBinding binding;
+
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe);
+        button = binding.chooseRecipesFloatingButton;
+        mDrawerLayout = (DrawerLayout) binding.getRoot();
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mDrawerLayout.openDrawer(GravityCompat.END);
     }
 }
